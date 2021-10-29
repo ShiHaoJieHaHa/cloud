169fbe33d52848e05f269967e88a7db65243fcad
shihaojie <dmlwxhn1314>
2021-10-29 16:48:03 +0800

    Person

---
+++ service-own-job/src/main/java/own/pojo/Person.java
@@ -0,0 +1,60 @@
+package own.pojo;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Person implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private Integer age;
+    @Column
+    private String address;
+
+    @Column
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
